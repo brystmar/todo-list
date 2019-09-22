b688ba948f976269fe0a2bbf45db016d476fb69e
Thomas <brystmar@users.noreply.github.com>
2019-09-21 17:04:03 -0700

    Phase 2: Basic structure & CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import TodoItem from './components/TodoItem'
+import './App.css'
 
 function App() {
-  return (
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt="logo" height="150" width="150"  />
+
+      <Header />
+
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+
+      <br/> <br/>
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className="navbar">
+            To-Do List
+        </header>
+    )
+}
+
+export default Header
+
